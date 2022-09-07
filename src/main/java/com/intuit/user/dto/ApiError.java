9e160829fc80ce236809e3c2e7d443c30d4e7835
Shelly Suri <shelly.suri99@gmail.com>
2022-09-07 15:56:21 +0530

    Add. lombok dependency

--- src/main/java/com/intuit/user/dto/ApiError.java
+++ src/main/java/com/intuit/user/dto/ApiError.java
@@ -1,52 +1,19 @@
 package com.intuit.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class ApiError {
     private LocalDateTime timestamp;
     private HttpStatus status;
     private String message;
     private List<String> errors;
-
-    public ApiError(LocalDateTime timestamp, HttpStatus status, String message, List<String> errors) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.message = message;
-        this.errors = errors;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setErrors(List<String> errors) {
-        this.errors = errors;
-    }
 }
--- src/main/java/com/intuit/user/model/User.java
+++ src/main/java/com/intuit/user/model/User.java
@@ -1,75 +1,32 @@
 package com.intuit.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
-    public User() {
-    }
-
-    public User(long id, String firstName, String lastName, String email, String password) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
-
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
