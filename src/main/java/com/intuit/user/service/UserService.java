9450b500dbf7487b7c7c55134f7a9bb3c49f6f38
Shelly Suri <shelly.suri99@gmail.com>
2022-09-08 07:24:01 +0530

    Add. Test cases for service and repository

--- src/main/java/com/intuit/user/service/UserService.java
+++ src/main/java/com/intuit/user/service/UserService.java
@@ -15,35 +15,45 @@ public class UserService {
     private final UserRepository userRepository;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public User create(String firstName, String lastName, String email, String password){
+    public User create(User user) {
+        Optional<User> userExists = userRepository.findByEmail(user.getEmail());
+        if (userExists.isPresent()) throw new UserAlreadyExistsException("User already exists with same email id.");
+
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+
+    public User create(String firstName, String lastName, String email, String password) {
         Optional<User> userExists = userRepository.findByEmail(email);
-        if(userExists.isPresent()) throw new UserAlreadyExistsException("User already exists with same email id.");
+        if (userExists.isPresent()) throw new UserAlreadyExistsException("User already exists with same email id.");
 
         String encodedPassword = bCryptPasswordEncoder.encode(password);
         User user = new User(firstName, lastName, email, encodedPassword);
         return userRepository.save(user);
     }
 
-    public User find(String email) throws UserNotFoundException{
-        Optional<User> user =  userRepository.findByEmail(email);
+    public User find(String email) throws UserNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
 
-        if(!user.isPresent()) throw new UserNotFoundException(String.format("No user exists with email : %s", email));
-        return  user.get();
+        if (!user.isPresent()) throw new UserNotFoundException(String.format("No user exists with email : %s", email));
+        return user.get();
     }
 
-    public User authenticate(String email, String password) throws InvalidCredentialsException{
-        Optional<User> user =  userRepository.findByEmail(email);
+    public User authenticate(String email, String password) throws InvalidCredentialsException {
+        Optional<User> user = userRepository.findByEmail(email);
 
-        if(!user.isPresent()) throw new UserNotFoundException(String.format("No user exists with email : %s", email));
+        if (!user.isPresent()) throw new UserNotFoundException(String.format("No user exists with email : %s", email));
 
         boolean passwordMatches = bCryptPasswordEncoder.matches(password, user.get().getPassword());
-        if(!passwordMatches) throw new InvalidCredentialsException("Invalid Credentials.");
+        if (!passwordMatches) throw new InvalidCredentialsException("Invalid Credentials.");
 
-        return  user.get();
+        return user.get();
     }
 }
--- src/test/java/com/intuit/user/service/UserServiceTest.java
+++ src/test/java/com/intuit/user/service/UserServiceTest.java
@@ -1,5 +1,8 @@
 package com.intuit.user.service;
 
+import com.intuit.user.exception.InvalidCredentialsException;
+import com.intuit.user.exception.UserAlreadyExistsException;
+import com.intuit.user.exception.UserNotFoundException;
 import com.intuit.user.model.User;
 import com.intuit.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -8,6 +11,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
@@ -26,7 +30,7 @@ class UserServiceTest {
     UserService userService;
 
     @Test
-    void shouldCreateUser(){
+    void shouldCreateUser() {
         User userCreated = new User("Eren", "Thomas", "e@agmail.com", "encoded-password");
         userCreated.setId(1L);
 
@@ -41,12 +45,53 @@ class UserServiceTest {
     }
 
     @Test
-    void shouldFindUserByEmail(){
-        User user = new User(1L,"Eren", "Thomas", "e@agmail.com", "password");
+    void shouldFindUserByEmail() {
+        User user = new User(1L, "Eren", "Thomas", "e@agmail.com", "password");
         when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
 
         User actualUser = userService.find("e@gmail.com");
+
         assertEquals(user, actualUser);
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailAlreadyExistsForNewUser() {
+        User user = new User(1L, "Eren", "Thomas", "e@agmail.com", "password");
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(new User()));
+
+        UserAlreadyExistsException actualException = assertThrows(
+                UserAlreadyExistsException.class,
+                () -> userService.create(user));
+
+        assertEquals("User already exists with same email id.", actualException.getMessage());
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailNotExists() {
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        UserNotFoundException actualException = assertThrows(
+                UserNotFoundException.class,
+                () -> userService.authenticate("example@example.com", "password")
+        );
+
+        assertTrue(actualException.getMessage().contains("No user exists with email"));
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
+    @Test
+    void shouldThrowExceptionForWrongPassword() {
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(new User()));
+        when(bCryptPasswordEncoder.matches(anyString(), anyString())).thenReturn(false);
+
+        InvalidCredentialsException actualException = assertThrows(
+                InvalidCredentialsException.class,
+                () -> userService.authenticate("example@example.com", "password")
+        );
+
+        assertEquals("Invalid Credentials.", actualException.getMessage());
         verify(userRepository, times(1)).findByEmail(anyString());
     }
 }
